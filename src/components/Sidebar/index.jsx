b49f7ff5ae626657999d1bc61d1d791813b81e0b
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-05-02 12:50:20 +0700

    KaiO

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -10,105 +10,90 @@ import { useContext, useState } from 'react';
 import { MyContext } from '../../App';
 import { TbCoins } from "react-icons/tb";
 import { LuUsers } from "react-icons/lu";
-import { BsHourglass } from "react-icons/bs";
 
 const Sidebar = () => {
-    const [activeTab, setActiveTab] = useState(0);
-    const [submenuState, setSubmenuState] = useState({});
-
-    const context = useContext(MyContext);
+    const [submenuState, setSubmenuState] = useState({}); // Trạng thái mở/đóng của submenu
 
     const toggleSubMenu = (index) => {
         setSubmenuState((prevState) => ({
             ...prevState,
-            [index]: !prevState[index],
+            [index]: !prevState[index], // Đảo trạng thái của submenu được nhấn
         }));
     };
 
     return (
-        <>
-            <div className="sidebar">
-                <ul>
-                    <li>
-                        <Link to='/dashboard'>
-                            <Button className={`w-100 ${activeTab === 0 ? 'active' : ''}`}>
-                                <span className='icon'><RiDashboardHorizontalLine /></span>
-                                Dashboard
-                                <span className='arrow'><FaAngleRight /></span>
-                            </Button>
-                        </Link>
-                    </li>
-                    <li>
-                        <Button
-                            className={`w-100 ${activeTab === 1 && submenuState[1] ? 'active' : ''}`}
-                            onClick={() => {
-                                setActiveTab(1);
-                                toggleSubMenu(1);
-                            }}
-                        >
-                            <span className='icon'><LuUsers /></span>
-                            User
+        <div className="sidebar">
+            <ul>
+                <li>
+                    <Link to='/dashboard'>
+                        <Button className="w-100">
+                            <span className='icon'><RiDashboardHorizontalLine /></span>
+                            Dashboard
                             <span className='arrow'><FaAngleRight /></span>
                         </Button>
-                        <div className={`submenuWrapper ${submenuState[1] ? 'colapse' : 'colapsed'}`}>
-                            <ul className='submenu'>
-                                <li><Link to="/user/list">User List</Link></li>
-                                <li><Link to="/user/view">User View</Link></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li>
-                        <Button
-                            className={`w-100 ${activeTab === 2 && submenuState[2] ? 'active' : ''}`}
-                            onClick={() => {
-                                setActiveTab(2);
-                                toggleSubMenu(2);
-                            }}
-                        >
-                            <span className='icon'><TbCoins /></span>
-                            Stake 
+                    </Link>
+                </li>
+                <li>
+                    <Button
+                        className={`w-100 ${submenuState[1] ? 'active' : ''}`}
+                        onClick={() => toggleSubMenu(1)}
+                    >
+                        <span className='icon'><LuUsers /></span>
+                        User
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                    <div className={`submenuWrapper ${submenuState[1] ? 'colapse' : 'colapsed'}`}>
+                        <ul className='submenu'>
+                            <li><Link to="/user/list">User List</Link></li>
+                            <li><Link to="/user/view">User View</Link></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <Button
+                        className={`w-100 ${submenuState[2] ? 'active' : ''}`}
+                        onClick={() => toggleSubMenu(2)}
+                    >
+                        <span className='icon'><TbCoins /></span>
+                        Stake 
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                    <div className={`submenuWrapper ${submenuState[2] ? 'colapse' : 'colapsed'}`}>
+                        <ul className='submenu'>
+                            <li><Link to="/stake/list">Stake List</Link></li>
+                            <li><Link to="/stake/view">Stake View</Link></li>
+                            <li><Link to="/stake/add">Stake Upload</Link></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <Button
+                        className={`w-100 ${submenuState[3] ? 'active' : ''}`}
+                        onClick={() => toggleSubMenu(3)}
+                    >
+                        <span className='icon'><BsCoin /></span>
+                        NFT 
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                    <div className={`submenuWrapper ${submenuState[3] ? 'colapse' : 'colapsed'}`}>
+                        <ul className='submenu'>
+                            <li><Link to="/nfts/list">NFT List</Link></li>
+                            <li><Link to="/nfts/view">NFT View</Link></li>
+                            <li><Link to="/nfts/add">NFT Upload</Link></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <Link to='/settings'>
+                        <Button className='w-100'>
+                            <span className='icon'><IoSettingsOutline /></span>
+                            Settings
                             <span className='arrow'><FaAngleRight /></span>
                         </Button>
-                        <div className={`submenuWrapper ${submenuState[2] ? 'colapse' : 'colapsed'}`}>
-                            <ul className='submenu'>
-                                <li><Link to="/stake/list">Stake List</Link></li>
-                                <li><Link to="/stake/view">Stake View</Link></li>
-                                <li><Link to="/stake/add">Stake Upload</Link></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li>
-                        <Button
-                            className={`w-100 ${activeTab === 3 && submenuState[3] ? 'active' : ''}`}
-                            onClick={() => {
-                                setActiveTab(3);
-                                toggleSubMenu(3);
-                            }}
-                        >
-                            <span className='icon'><BsCoin  /></span>
-                            NFT 
-                            <span className='arrow'><FaAngleRight /></span>
-                        </Button>
-                        <div className={`submenuWrapper ${submenuState[3] ? 'colapse' : 'colapsed'}`}>
-                            <ul className='submenu'>
-                                <li><Link to="/nfts/list">NFT List</Link></li>
-                                <li><Link to="/nfts/view">NFT View</Link></li>
-                                <li><Link to="/nfts/add">NFT Upload</Link></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li>
-                        <Link to='/settings'>
-                            <Button className='w-100'>
-                                <span className='icon'><IoSettingsOutline /></span>
-                                Settings
-                                <span className='arrow'><FaAngleRight /></span>
-                            </Button>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </>
+                    </Link>
+                </li>
+            </ul>
+        </div>
     );
 };
 
